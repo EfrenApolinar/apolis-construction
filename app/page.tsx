592f9c8ff104fed12f11b75a1dd36faa7028899c
Black-Meteorite <efrenapolinar@gmail.com>
2026-08-27 15:26:22 -0700

    Created homepage
    Updated the homepage to a more modern aesthetic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
 export default function Home(){
   return(
-    <main className = "min-h-screen flex items-center justify-center bg-zinc-900">
-      <h1 className="text-5x1 font-bold text-white">
-        Apolis Construction
-      </h1>
-      
+    <main>
+      {/*Hero*/}
+        <section className = "relative min-h-screen flex items-center justify-center">
+          {/*Background photo */}
+          <img
+          src = "/hero.jpg"
+          alt =""
+          className = "absolute inset-0 w-full h-full object-cover"
+          />
+
+          {/*Dark Overlay */}
+          <div className = "absolute inset-0 bg-black/50"/> 
+
+          {/*Content */}
+          <div className ="relative z-10 text-center px-6 max-w-3xl">
+            <h1 className = "text-6xl md:text-8xl font-extrabold uppercase text-white leading-none tracking-tight">
+              uhh. I'll think of one
+            </h1>
+            <p className= "text-white mt-6 max-w-md mx-auto">
+              Quality work. Serving the Greater Los Angeles area since 2004
+            </p>
+            
+            <a href="#contact"
+            className = "inline-block mt-10 bg-zinc-100 hover:bg-white text-zinc-900 font-semibold text-sm tracking-wide uppercase px-10 py-4 rounded-full transition-colors"
+            >
+              Get Started
+            </a>
+            </div>
+        </section>
     </main>
   )
 }
